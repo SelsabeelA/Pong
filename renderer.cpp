e2aaf2e5c070c9167205acfd5aab46adc858d315
Selsabeel A <90451756+SelsabeelA@users.noreply.github.com>
2022-09-19 13:34:40 +0300

    Add files via upload

---
+++ renderer.cpp
@@ -0,0 +1,57 @@
+internal void render_background() {
+	u32* pixel = (u32*)render_state.memory;
+	for (int y = 0; y < render_state.height; y++) {
+		for (int x = 0; x < render_state.width; x++) {
+			*pixel++ = x;
+		}
+	}
+}
+
+internal void clear_screen(u32 color) {
+	u32* pixel = (u32*)render_state.memory;
+	for (int y = 0; y < render_state.height; y++) {
+		for (int x = 0; x < render_state.width; x++) {
+			*pixel++ = color;
+		}
+	}
+
+}
+
+
+internal void draw_rect_in_pixels(int x1, int y1, int x2, int y2, u32 color){	
+	x1 = clamp(x1, 0, render_state.width);
+	x2 = clamp(x2, 0, render_state.width);
+	y1 = clamp(y1, 0, render_state.height);
+	y2 = clamp(y2, 0, render_state.height);
+
+	for (int y = y1; y < y2; y++) {
+		u32* pixel = (u32*)render_state.memory + x1 + y*render_state.width;
+		for (int x = x1; x < x2; x++) {
+			*pixel++ = color;
+		}
+	}
+}
+
+
+global_variable float render_scale = 0.01f;
+
+
+
+internal void draw_rect(float x, float y, float half_size_x, float half_size_y, u32 color) {
+
+	x *= render_state.height * render_scale;
+	y *= render_state.height * render_scale;
+	half_size_x *= render_state.height * render_scale;
+	half_size_y *= render_state.height * render_scale;
+
+	x += render_state.width / 2.f;
+	y += render_state.height / 2.f;
+
+	// Change to pixels
+	int x0 = x - half_size_x;
+	int x1 = x + half_size_x;
+	int y0 = y - half_size_y;
+	int y1 = y + half_size_y;
+
+	draw_rect_in_pixels(x0, y0, x1, y1, color);
+}
---
+++ win32_platform.cpp
@@ -0,0 +1,107 @@
+#include "utilities.cpp"
+#include <Windows.h>
+
+global_variable bool window_running = true;
+
+struct RenderState {
+    int height, width;
+    void* memory;
+    BITMAPINFO bitmap_info;
+};
+global_variable RenderState render_state;
+//COLORS
+#define red 0xff5500
+
+#include "renderer.cpp"
+
+LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    LRESULT result = 0;
+
+    switch (uMsg)
+    {
+    case WM_CLOSE:
+
+    case WM_DESTROY:
+    {
+        window_running = false;
+    }
+    break;
+
+    case WM_SIZE:
+    {
+        RECT rect;
+        GetClientRect(hwnd, &rect);
+        render_state.width = rect.right - rect.left;
+        render_state.height = rect.bottom - rect.top;
+
+        int size = render_state.width * render_state.height * sizeof(unsigned int);
+
+        if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
+        render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+        render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+        render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+        render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+        render_state.bitmap_info.bmiHeader.biPlanes = 1; 
+        render_state.bitmap_info.bmiHeader.biBitCount = 32;
+        render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+    }
+    break;
+
+    default:
+    {
+        result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    }
+    return result;
+}
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    //create window class
+        // a window class is a set of behaviours that windows might have in common
+    // like for eg. with buttons clicking on the button gives the same result across different windows
+    // data that is unique to each window is called instance data
+    WNDCLASSW window_class = {}; //changed from WNDCLASS
+    window_class.style = CS_HREDRAW | CS_VREDRAW;       //draw horizontally and vertically 
+    
+    // Window classes are registered with the system at run time
+    // to register them we fill in a WNDCLASS structure as below
+    
+    
+    window_class.lpszClassName = L"Game Window Class";
+    window_class.lpfnWndProc = window_callback;
+    // pointer to a callback function called window procedure,
+    // used by the window to send messages to us
+ 
+
+
+    //Register class
+    RegisterClass(&window_class);
+
+    //create 
+    HWND window = CreateWindow(window_class.lpszClassName, L"Game Window Name..", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+    HDC hdc = GetDC(window);
+
+
+    while (window_running)
+    {
+        //input
+        MSG message;
+        while (PeekMessage(&message, window, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&message);
+            DispatchMessage(&message);
+        }
+
+        //simulate
+        clear_screen(red);
+        //render_background();
+        draw_rect(-2, 0, 8, 3, 0xff22);
+        //render
+        StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+    }
+
+}
